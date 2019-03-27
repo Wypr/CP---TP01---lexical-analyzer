3b21e20871dbd79dc5c26fd43f2dc557f018f5ef
Wadson Pereira <wadson.ayres@gmail.com>
2019-03-26 22:41:21 -0300

    modelo inicial

--- lex.rb
+++ lex.rb
@@ -1,9 +1,18 @@
 require 'lex'
+#documentaçaõ da GEM: https://github.com/piotrmurach/lex
 
 class PascalLanguage < Lex::Lexer
-
+    tokens(
+        :NUM,
+        :FLOAT_NUM,
+        :ID,
+        :STRING
+      )
+    rule(:ID, /\A[_\$a-zA-Z][_\$0-9a-zA-Z]*/)
 end
 
-
-#x = PascalLanguage.new
-puts "OKOK"
+=begin
+input = arquivo vindo do cmd
+teste = PascalLanguage.new
+output = teste.lex(input)  #output será um vetor de tokens 
+=end
